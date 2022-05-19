a22f25386946371771a1c9c59b8f61883b6897e0
joao.zanutto <joao.zanutto@orbee.com>
2022-05-19 16:29:13 -0300

    adding host and port to run in docker

--- TechnicalInterviewUI/Static/EndPoints.cs
+++ TechnicalInterviewUI/Static/EndPoints.cs
@@ -2,7 +2,7 @@
 {
     public class EndPoints
     {
-        public static string BaseUrl = "https://localhost:44363/"; //You will need to change to your reality
+        public static string BaseUrl = "http://host.docker.internal:5000/"; //You will need to change to your reality
         public static string DocumentValidatorEndpoint = $"{BaseUrl}api/DocumentValidator/";
     }
 }
